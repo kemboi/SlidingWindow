5c09599a8a7588d50005808f60b679377c332ddc
Evans Kemboi <evanskemb@gmail.com>
2023-02-04 15:10:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Sliding Windows Algorithm");
+
+    int[] arr = { 10, 4, 5, 2, 3, 6, 7, 20, 3, 4, 5, 2, 1, 8 };
+    int size = 2;
+
+    String output = String.format("The maximum subarray of size %d is %d", size, maxSum(arr, size));
+    System.out.println(output);
+  }
+
+  public static int maxSum(int[] arr, int size) {
+    int maxsum = 0;
+    int windowsum = 0;
+    for (int index = 0; index < size; index++) {
+      windowsum += arr[index];
+      maxsum = windowsum;
+    }
+    for (int i = size; i < arr.length - size; i++) {
+      windowsum += arr[i] - arr[i - size];
+      maxsum = Math.max(windowsum, maxsum);
+    }
+    return maxsum;
+  }
+
+}
